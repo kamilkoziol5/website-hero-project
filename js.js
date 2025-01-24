2611d93061c6d9f6d70d4cb84d06ec4baf2cc73b
Kamil <kamilkoziol529@gmail.com>
2025-01-24 11:19:33 +0100

    first commit

---
+++ js.js
@@ -0,0 +1,10 @@
+
+function openSidebar() {
+	const sidebar = document.querySelector(".sidebar")
+	sidebar.classList.add("open")
+}
+
+function closeSidebar() {
+	const sidebar = document.querySelector(".sidebar")
+	sidebar.classList.remove("open")
+}
